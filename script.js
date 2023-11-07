557bbdc70174840d00c5bb7b775e6889c7389fa6
Pieter <pieter@sulman.net>
2023-11-07 01:08:27 -0500

    added arrow functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputElem = document.querySelector('.todoInput');
 const todoDivElem = document.querySelector('.js-todo-list');
 const inputDueDate = document.querySelector('.js-due-date');
+const addButton = document.querySelector('.add-button');
 
 let todoList = [
     { todoName: 'foo', dueDate: '01/01/0101' },
@@ -10,27 +11,20 @@ let todoList = [
 
 populateList();
 
-inputElem.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        addToList();
-    }
-});
-
-inputDueDate.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        addToList();
-    }
-});
+addButton.addEventListener("click", () => { addToList() });
+inputElem.addEventListener("keypress", event => { event.key === "Enter" && addToList() });
+inputDueDate.addEventListener("keypress", event => { event.key === "Enter" && addToList() });
 
 function addToList() {
     const todoName = inputElem.value;
     const dueDate = inputDueDate.value;
     const listArrayObj = { todoName, dueDate };
-    todoName && todoList.push(listArrayObj);
+    // todoName && 
+    todoList.push(listArrayObj);
     inputElem.value = '';
     inputDueDate.value = '';
     populateList();
-};
+}
 
 function populateList() {
     let todoListHTML = ''
@@ -38,16 +32,17 @@ function populateList() {
         const todoObject = todoList[i];
         const { todoName } = todoObject;
         const { dueDate } = todoObject;
-        const todoPara = `
+        const todoHTMLElem = `
             <div class="todo-name">${todoName}</div> 
             <div class="todo-date">${dueDate}</div>
             <button onclick="deleteItem(${i})" class="delete-button">Delete</button>`;
-        todoListHTML += todoPara;
+        todoListHTML += todoHTMLElem;
     };
     todoDivElem.innerHTML = todoListHTML;
-};
+    // why no declare needed?
+    deleteButtons = document.querySelectorAll('.delete-button');
+}
 
-function deleteItem(arrayIndex) {
-    todoList.splice(arrayIndex, 1);
-    populateList();
-};
+const deleteItem = arrayIndex => { todoList.splice(arrayIndex, 1); populateList(); }
+
+console.log(deleteButtons);
